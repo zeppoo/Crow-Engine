fedca276d2630117eddda6e2168d51a698a407b7
Rein V <reinverhaag@gmail.com>
2025-01-12 22:26:44 +0100

    command buffer safety

--- CrowEngine/include/Vulkan/CommandBuffers.hpp
+++ CrowEngine/include/Vulkan/CommandBuffers.hpp
@@ -1,37 +1,38 @@
 #pragma once
-#include "crow_lib.hpp"
+#include "Logger.hpp"
 #include "Vulkan/Device.hpp"
 #include "Vulkan/QueueManager.hpp"
-#include "Logger.hpp"
+#include "crow_lib.hpp"
 
-namespace vulkan {
-  struct CommandBuffer {
-    CommandBuffer(VkCommandBuffer* buffer, QueueType type);
-    ~CommandBuffer();
+namespace vulkan
+{
+struct CommandBuffer {
+  CommandBuffer(VkCommandBuffer *buffer, QueueType type);
+  ~CommandBuffer();
 
-    void BeginRecording();
-    void EndRecording();
-    void Reset();
+  void BeginRecording();
+  void EndRecording();
+  void Reset();
 
-    VkCommandBuffer* buffer;
-    QueueType bufferType;
-    bool isRecorded = false;
-  };
+  VkCommandBuffer *buffer;
+  QueueType bufferType;
+  bool isRecorded = false;
+};
 
-  class CommandBufferManager {
-  public:
-    CommandBufferManager(std::unique_ptr<Device>& device, std::unique_ptr<QueueManager>& queueManager);
+class CommandBufferManager {
+public:
+  CommandBufferManager(std::unique_ptr<Device> &device, std::unique_ptr<QueueManager> &queueManager);
 
-    CommandBuffer CreateBuffer(QueueType bufferType);
-    void QueueBuffer(CommandBuffer commandBuffer);
-    void AddBuffer(CommandBuffer commandBuffer);
-    void RemoveBuffer(int index);
-    void ResetStoredBuffers();
-    void SubmitStoredBuffers();
+  CommandBuffer CreateBuffer(QueueType bufferType);
+  void QueueBuffer(CommandBuffer commandBuffer);
+  void AddBuffer(CommandBuffer commandBuffer);
+  void RemoveBuffer(int index);
+  void ResetStoredBuffers();
+  void SubmitStoredBuffers();
 
-  private:
-    std::vector<CommandBuffer> reuseBuffers;
-    std::unique_ptr<Device>& device;
-    std::unique_ptr<QueueManager>& queueManager;
-  };
-}
+private:
+  std::vector<CommandBuffer> reuseBuffers;
+  std::unique_ptr<Device> &device;
+  std::unique_ptr<QueueManager> &queueManager;
+};
+} // namespace vulkan
--- CrowEngine/include/Vulkan/QueueManager.hpp
+++ CrowEngine/include/Vulkan/QueueManager.hpp
@@ -1,101 +1,91 @@
 #pragma once
 
-#include "crow_lib.hpp"
+#include <functional>
 #include <optional>
+#include "crow_lib.hpp"
 
 namespace vulkan
 {
-  enum QueueType {
-    PRESENT,
-    GRAPHICS,
-    COMPUTE,
-    TRANSFER
-  };
+enum QueueType { PRESENT, GRAPHICS, COMPUTE, TRANSFER };
 
-  struct QueueData {
-    VkFence queueFence;
-    std::vector<VkCommandBuffer> recordedBuffers;
-    VkQueue *pQueue;
-    int queueIndex;
-    int familyIndex;
+struct QueueData {
+  VkFence queueFence;
+  std::vector<VkCommandBuffer> recordedBuffers;
+  VkQueue *pQueue;
+  int queueIndex;
+  int familyIndex;
 
-    void SubmitBuffers(VkDevice device)
-    {
-      vkWaitForFences(device, 1, &queueFence, VK_TRUE, UINT64_MAX); // Wait for previous frame
-      vkResetFences(device, 1, &queueFence);  // Reset for reuse
-      VkSubmitInfo submitInfo{};
-      submitInfo.commandBufferCount = recordedBuffers.size();
-      submitInfo.pCommandBuffers = recordedBuffers.data();
-      vkQueueSubmit(*pQueue, 1, &submitInfo, queueFence);
-    }
-  };
+  void SubmitBuffers(VkDevice device)
+  {
+    vkWaitForFences(device, 1, &queueFence, VK_TRUE, UINT64_MAX); // Wait for previous frame
+    vkResetFences(device, 1, &queueFence); // Reset for reuse
+    VkSubmitInfo submitInfo{};
+    submitInfo.commandBufferCount = recordedBuffers.size();
+    submitInfo.pCommandBuffers = recordedBuffers.data();
+    vkQueueSubmit(*pQueue, 1, &submitInfo, queueFence);
+  }
+};
 
-  struct QueueFamily {
-    int index;
-    int queueCount = 0;
-    std::vector<VkQueue> queues;
-    std::vector<float> queuePriorities;
-    VkCommandPool commandPool = VK_NULL_HANDLE;
-    std::vector<VkCommandBuffer> commandBuffers = {VK_NULL_HANDLE};
-  };
+struct QueueFamily {
+  int index;
+  int queueCount = 0;
+  std::vector<VkQueue> queues;
+  std::vector<float> queuePriorities;
+  VkCommandPool commandPool = VK_NULL_HANDLE;
+  std::vector<VkCommandBuffer> commandBuffers = {VK_NULL_HANDLE};
+  std::vector<VkCommandBuffer *> availableBuffers;
+  std::vector<VkCommandBuffer *> inUseBuffers;
+};
 
-  class QueueManager {
-  public:
-    void FindQueueFamilies(VkPhysicalDevice &physicDevice, VkSurfaceKHR &surface);
+class QueueManager {
+public:
+  void FindQueueFamilies(VkPhysicalDevice &physicDevice, VkSurfaceKHR &surface);
 
-    void AllocateQueues(VkDevice &device);
+  void AllocateQueues(VkDevice &device);
 
-    bool CreateCommandPools(VkDevice &device);
+  bool CreateCommandPools(VkDevice &device);
 
-    bool AllocateCommandBuffers(VkDevice &device);
+  bool AllocateCommandBuffers(VkDevice &device);
 
-    std::vector<VkDeviceQueueCreateInfo> CreateQueueInfos();
+  std::vector<VkDeviceQueueCreateInfo> CreateQueueInfos();
 
-    VkCommandBuffer* GetCommandBuffer(QueueType bufferType);
+  VkCommandBuffer *GetCommandBuffer(QueueType bufferType);
+  void ReturnCommandBuffer(VkCommandBuffer *buffer, QueueType bufferType);
 
-    void SubmitCommandBuffer(VkCommandBuffer* commandBuffer, QueueType queueType);
+  void SubmitCommandBuffer(VkCommandBuffer *commandBuffer, QueueType queueType);
 
-    std::vector<QueueFamily> GetQueueFamilies()
-    { return queueFamilies; }
+  std::vector<QueueFamily> GetQueueFamilies() { return queueFamilies; }
 
-    std::vector<QueueData> GetPresentQueues()
-    { return presentQueues; }
+  std::vector<QueueData> GetPresentQueues() { return presentQueues; }
 
-    std::vector<QueueData> GetGraphicsQueues()
-    { return graphicsQueues; }
+  std::vector<QueueData> GetGraphicsQueues() { return graphicsQueues; }
 
-    std::vector<QueueData> GetComputeQueues()
-    { return computeQueues; }
+  std::vector<QueueData> GetComputeQueues() { return computeQueues; }
 
-    std::vector<QueueData> GetTransferQueues()
-    { return transferQueues; }
+  std::vector<QueueData> GetTransferQueues() { return transferQueues; }
 
-  private:
-    void AssignQueuesToQueueFamilies(
-        VkPhysicalDevice &physicDevice,
-        VkSurfaceKHR &surface,
-        VkQueueFlagBits flagBit,
-        std::vector<QueueFamily> &currentQueueFamilies,
-        const std::vector<VkQueueFamilyProperties> &queueFamiliesProperties,
-        std::vector<QueueData> &queueType,
-        int indicies, bool isPresentQueue);
+private:
+  void AssignQueuesToQueueFamilies(VkPhysicalDevice &physicDevice, VkSurfaceKHR &surface, VkQueueFlagBits flagBit,
+                                   std::vector<QueueFamily> &currentQueueFamilies,
+                                   const std::vector<VkQueueFamilyProperties> &queueFamiliesProperties, std::vector<QueueData> &queueType,
+                                   int indicies, bool isPresentQueue);
 
-    QueueData CreateQueueData(QueueFamily &family, int maxQueueCount);
+  QueueData CreateQueueData(QueueFamily &family, int maxQueueCount);
 
-    void CleanupEmptyFamilies(std::vector<QueueFamily> &currentQueueFamilies);
+  void CleanupEmptyFamilies(std::vector<QueueFamily> &currentQueueFamilies);
 
-    int CheckFlagSupportNum(VkQueueFlags flags);
+  int CheckFlagSupportNum(VkQueueFlags flags);
 
-    void GetQueueHandles(VkDevice &device);
+  void GetQueueHandles(VkDevice &device);
 
-    void BindQueueDataToQueues(std::vector<QueueData> &queueList);
+  void BindQueueDataToQueues(std::vector<QueueData> &queueList);
 
-    void CreateQueueFences(VkDevice &device, std::vector<QueueData> &queueList);
+  void CreateQueueFences(VkDevice &device, std::vector<QueueData> &queueList);
 
-    std::vector<QueueFamily> queueFamilies;
-    std::vector<QueueData> presentQueues;
-    std::vector<QueueData> graphicsQueues;
-    std::vector<QueueData> computeQueues;
-    std::vector<QueueData> transferQueues;
-  };
-}
+  std::vector<QueueFamily> queueFamilies;
+  std::vector<QueueData> presentQueues;
+  std::vector<QueueData> graphicsQueues;
+  std::vector<QueueData> computeQueues;
+  std::vector<QueueData> transferQueues;
+};
+} // namespace vulkan
